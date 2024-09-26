8e7d5fbcf2147f358b7fdee4cb579fd628e9d0e3
Jontez Snider <jlsnider3579@gmail.com>
2024-09-26 10:14:29 -0400

    completed ShoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.Arrays;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("1.milk 2.cereal 3.eggs 4.chicken 5.burgers 6.yams 7.turkey 8.juice 9.fruit 10.toothpaste");
+
+    }
+}
